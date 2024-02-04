be88b1d617b6a0ba3aac70c4dd43b99ff5237bd2
DevSalmanAli <158503239+DevSalmanAli@users.noreply.github.com>
2024-02-05 00:28:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    var message = "Zindagi Har Pal Kuchh Khaas Nahi Hoti,Phoolo Ki Khushboo Hamesha Paas Nahi Hoti,    Milna Humari Takdeer Mein Tha Varna,    Itni Pyari Dosti ittefaaq Nahi Hoti";
+    "Happy ";
+    document.getElementById("specialMessage").innerHTML = message;
+  }
+  
